6ffcc89511eb7cefa5ea905fd98e25a80677f56e
terran-king <terranlking@gmail.com>
2023-10-03 16:29:09 -0700

    Added Post functionality to add a student to the database

--- src/main/java/com/example/SpringbootLearning/student/StudentController.java
+++ src/main/java/com/example/SpringbootLearning/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.SpringbootLearning.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -24,4 +22,12 @@ public class StudentController {
     public List<Student> getStudents() {
        return studentService.getStudents();
     }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+
+
 }
--- src/main/java/com/example/SpringbootLearning/student/StudentService.java
+++ src/main/java/com/example/SpringbootLearning/student/StudentService.java
@@ -3,9 +3,8 @@ package com.example.SpringbootLearning.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,5 +19,16 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
+
     }
 }
